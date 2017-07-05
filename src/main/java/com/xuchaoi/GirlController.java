a0fba1cba79423a30b2bd43f397288ff57e0da59
xuchao <xuchao@rabbit.com>
2017-07-05 10:59:58 +0800

    新增对Girl增删改查接口

---
+++ src/main/java/com/xuchaoi/GirlController.java
@@ -0,0 +1,85 @@
+package com.xuchaoi;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class GirlController {
+
+    @Autowired
+    private GirlRepository girlRepository;
+
+    /**
+     * 查询所有女生列表
+     * @return
+     */
+    @GetMapping(value = "/girls")
+    public List<Girl> girlList() {
+        return girlRepository.findAll();
+    }
+
+    /**
+     * 按年龄查询女生列表
+     * @param age
+     * @return
+     */
+    @GetMapping(value = "/girls/age/{age}")
+    public List<Girl> girlListByAge(@PathVariable("age") Integer age) {
+        return girlRepository.findByAge(age);
+    }
+
+    /**
+     * 添加一个女生
+     * @param cupSize
+     * @param age
+     * @return
+     */
+    @PostMapping(value = "/girls")
+    public Girl girlAdd(@RequestParam("cupSize") String cupSize,
+                          @RequestParam("age") Integer age) {
+        Girl girl = new Girl();
+        girl.setCupSize(cupSize);
+        girl.setAge(age);
+
+        return girlRepository.save(girl);
+    }
+
+    /**
+     * 查询一个女生
+     * @param id
+     * @return
+     */
+    @GetMapping(value = "/girls/{id}")
+    public Girl girlFindOne(@PathVariable("id") Integer id) {
+        return girlRepository.findOne(id);
+    }
+
+    /**
+     * 更新一个女生
+     * @param id
+     * @return
+     */
+    @PutMapping(value = "/girls/{id}")
+    public Girl girlUpdate(@PathVariable("id") Integer id,
+                           @RequestParam("cupSize") String cupSize,
+                           @RequestParam("age") Integer age) {
+        Girl girl = new Girl();
+        girl.setId(id);
+        girl.setCupSize(cupSize);
+        girl.setAge(age);
+
+        return girlRepository.save(girl);
+    }
+
+    /**
+     * 删除一个女生
+     * @param id
+     */
+    @DeleteMapping(value = "/girls/{id}")
+    public void girlDelete(@PathVariable("id") Integer id) {
+        girlRepository.delete(id);
+    }
+
+}
